b2c47c5c591403fbf5cc464d83a8a6393ce836f2
Arshal Rejith <iamarshalrejith@gmail.com>
2025-07-01 13:02:16 +0530

    Update components and add new pokemon data

--- src/components/TypeCard.jsx
+++ src/components/TypeCard.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import { pokemonTypeColors } from "../utils"
 
-const TypeCard = () => {
-  return (
-    <div>TypeCard</div>
-  )
+export default function TypeCard(props) {
+    const { type } = props
+    return (
+        <div className="type-tile" style={{ color: pokemonTypeColors?.[type]?.color, background: pokemonTypeColors?.[type]?.background }}>
+            <p>{type}</p>
+        </div>
+    )
 }
-
-export default TypeCard
